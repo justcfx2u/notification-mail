b6f7ef8c936cb82fb3a3fe7caca7a3c42cd140e3
Bart Vrancken <bart@abuse.io>
2015-11-09 00:32:20 +0100

    implement notifications by mail

--- config/Mail.php
+++ config/Mail.php
@@ -1,5 +1,49 @@
 <?php
 
 return [
+    'templates' => [
+
+        'ip_box'                        => '
+[Ticket <<TICKET_NUMBER>>] Reporting <<TICKET_TYPE_NAME>> (<<TICKET_CLASS_NAME>>) for IP addres: <<TICKET_IP>>
+<<TICKET_TYPE_DESCRIPTION>>
+More information at: <<IP_CONTACT_ASH_LINK>>
+                                           ',
+
+        'domain_box'                    => '
+[Ticket <<TICKET_NUMBER>>] Reporting <<TICKET_TYPE_NAME>> (<<TICKET_CLASS_NAME>>) for domain name: <<TICKET_DOMAIN>>
+<<TICKET_TYPE_DESCRIPTION>>
+More information at: <<DOMAIN_CONTACT_ASH_LINK>>
+                                           ',
+
+        'mail'                          => '
+Dear Customer,
+
+You have received this report because the <<COUNT>> IP address(es) linked to this report is
+under your control. We ask you to examine this report and take the necessary action(s)
+
+After resolving the matter we like to receive feedback from you which measures you
+have taken to resolve and prevent new reports. You can leave your feedback by clicking
+the URL next to each report. The portal URL also contains additional information how
+to solve the problem this reports entails.
+
+<<BOXES>>
+
+Some reports are not considered abuse but more informational to the fact you are running
+a service that might be susceptible to abuse in the feature. While we believe preventing
+abuse is better then resolving it, we ask you to take measures to prevent actual abuse of
+your service. However if you feel this service is running as intended you can choose to
+ignore it. Warning: Ignoring actual abuse reports could lead to actions listed in our
+Abuse Policy or Acceptable Use Policy!
+
+All the information we have on report(s) are listed in in the portal. In most cases we
+received this report externally and do not have any more information nor can we put
+you in contact with the original reporter.
+
+With regards,
+
+ISP Abuse Department
+                                           ',
+
+    ],
 
 ];
--- src/Mail.php
+++ src/Mail.php
@@ -2,6 +2,8 @@
 
 namespace AbuseIO\Notification;
 
+use Config;
+
 class Mail extends Notification
 {
     /**
@@ -13,11 +15,50 @@ class Mail extends Notification
     }
 
     /**
-     * Sends out mail notifications
+     * Sends out mail notifications for a specific $customerReference
      * @return boolean  Returns if succeeded or not
      */
-    public function send()
+    public function send($notifications)
     {
+        // TODO - Fix config service provider (move parser one into laravel default?)
+        Config::set('notifications.mail', include('/opt/abuseio/vendor/abuseio/notification-mail/config/Mail.php'));
+
+        foreach ($notifications as $customerReference => $notificationTypes) {
+            foreach ($notificationTypes as $notificationType => $tickets) {
+                $boxes = [ ];
+                foreach ($tickets as $ticket) {
+                    $replacements = [
+                        'IP_CONTACT_ASH_LINK'       => config('main.ash.url') . 'collect/' .
+                            md5($ticket->id . $ticket->ip . $ticket->ip_contact_reference),
+                        'DOMAIN_CONTACT_ASH_LINK'   => config('main.ash.url') . 'collect/' .
+                            md5($ticket->id . $ticket->ip . $ticket->domain_contact_reference),
+                        'TICKET_NUMBER'             => $ticket->id,
+                        'TICKET_IP'                 => $ticket->ip,
+                        'TICKET_DOMAIN'             => $ticket->domain,
+                        'TICKET_TYPE_NAME'          => trans("types.type.{$ticket->type_id}.name"),
+                        'TICKET_TYPE_DESCRIPTION'   => trans("types.type.{$ticket->type_id}.description"),
+                        'TICKET_CLASS_NAME'         => trans("classifications.{$ticket->class_id}.name"),
+                        'TICKET_EVENT_COUNT'        => $ticket->events->count(),
+                    ];
+
+                    $box = config("notifications.mail.templates.{$notificationType}_box");
+
+                    foreach ($replacements as $search => $replacement) {
+                        $box = str_replace("<<{$search}>>", $replacement, $box);
+                    }
+
+                    $boxes[] = $box;
+                }
+            }
+        }
+
+        if (!empty($boxes)) {
+            $boxes = trim(implode('bart', $boxes));
+            $mail = config("notifications.mail.templates.mail");
+            $mail = str_replace('<<BOXES>>', $boxes, $mail);
+
+            print_r($mail);
+        }
 
         return true;
     }
